639e688544a7c48773529be830dd5726756a67c3
hmursaleen <habibulmursaleen@gmail.com>
2023-06-12 01:51:34 +0600

    updated CSS, more needed

--- beebay/urls.py
+++ beebay/urls.py
@@ -21,10 +21,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
